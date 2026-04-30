34e17619682afcbae36ded3624aaab19d67397ab
sihumin <sihumin196@gmail.com>
2026-04-30 09:31:06 +0900

    Delete scripts directory

--- scripts/run_env_info.ts
+++
@@ -1,4 +0,0 @@
-import { spawn } from 'child_process';
-const proc = spawn('python3', ['scripts/env_info.py']);
-proc.stdout.on('data', (d) => process.stdout.write(d));
-proc.stderr.on('data', (d) => process.stderr.write(d));
